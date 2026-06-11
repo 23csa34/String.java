41835abc9330265be00b4673aa1f8cadec9bae26
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-11 21:40:45 +0530

    Add ReverseString.java to print reversed string

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n =5;
+        String s = "hello";
+
+        for(int i = n-1; i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
